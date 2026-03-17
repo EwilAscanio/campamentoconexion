613b56063c4aabfa9d2d9db65919ddb1abd94e8a
Ewil Ascanio <ewilascanio@gmail.com>
2026-03-17 14:29:38 -0400

    Creacion de Pagina de Juegos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -106,12 +106,12 @@ const Navbar = () => {
                 {item.label}
               </Link>
             ))}
-            <Link
+            {/* <Link
               href="/juegos"
               className="px-4 py-2 text-sm font-semibold text-foreground/80 hover:text-primary transition-colors rounded-lg hover:bg-primary/10"
             >
               Juegos
-            </Link>
+            </Link> */}
           </div>
 
           {/* CTA Button (Desktop) */}
